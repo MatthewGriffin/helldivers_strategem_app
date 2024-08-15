5ca97a4f2bd40dd50e969a643535ec553536922a
MatthewGriffin <demengies@gmail.com>
2024-08-15 13:51:56 +0100

    track start end of touch

--- src/touch_detection.rs
+++ src/touch_detection.rs
@@ -1,22 +1,34 @@
-use bevy::prelude::{EventReader, TouchInput};
+use bevy::{
+    math::{Quat, Vec2, Vec3},
+    prelude::{EventReader, TouchInput},
+};
 
 pub fn touch_detection(mut touch_evr: EventReader<TouchInput>) {
     use bevy::input::touch::TouchPhase;
     for ev in touch_evr.read() {
+        let mut start = Vec3::new(0.0, 0.0, 1.0);
+        let mut end = Vec3::new(0.0, 0.0, 1.0);
         // in real apps you probably want to store and track touch ids somewhere
         match ev.phase {
             TouchPhase::Started => {
                 println!("Touch {} started at: {:?}", ev.id, ev.position);
+                start.x = ev.position.x;
+                start.y = ev.position.y;
             }
             TouchPhase::Moved => {
                 println!("Touch {} moved to: {:?}", ev.id, ev.position);
             }
             TouchPhase::Ended => {
                 println!("Touch {} ended at: {:?}", ev.id, ev.position);
+                end.x = ev.position.x;
+                end.y = ev.position.y;
             }
             TouchPhase::Canceled => {
                 println!("Touch {} cancelled at: {:?}", ev.id, ev.position);
             }
         }
+
+        let angle = Quat::from_rotation_arc(Vec3::Y, (start - end).normalize());
+        println!("final angle is {}", angle);
     }
 }
