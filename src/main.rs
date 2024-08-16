96e4e17410fd79134a0b1ed7fee7871744f81a1b
MatthewGriffin <demengies@gmail.com>
2024-08-16 17:30:10 +0100

    ui init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 mod process_input;
 mod process_touch_event;
+mod setup_interface;
 mod touch_detection;
 mod touch_resource;
 
 use bevy::{
-    app::{App, Update},
+    app::{App, Startup, Update},
+    prelude::{default, PluginGroup},
+    window::{Window, WindowMode, WindowPlugin},
+    winit::WinitSettings,
     DefaultPlugins,
 };
 use process_touch_event::ProcessTouchEvent;
+use setup_interface::setup_interface;
 use touch_resource::TouchResource;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(WinitSettings::desktop_app())
         .add_systems(
             Update,
             (
@@ -20,6 +32,7 @@ fn main() {
                 process_input::process_input,
             ),
         )
+        .add_systems(Startup, setup_interface)
         .insert_resource(TouchResource::default())
         .add_event::<ProcessTouchEvent>()
         .run();
---
+++ src/setup_interface.rs
@@ -0,0 +1,67 @@
+use bevy::{
+    color::Color,
+    prelude::{default, BuildChildren, Camera2dBundle, Commands, NodeBundle},
+    ui::{BackgroundColor, Display, GridTrack, Style, Val},
+};
+
+pub fn setup_interface(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                display: Display::Grid,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                grid_template_columns: vec![GridTrack::flex(1.0)],
+                grid_template_rows: vec![
+                    GridTrack::flex(0.1),
+                    GridTrack::flex(0.1),
+                    GridTrack::flex(0.6),
+                    GridTrack::flex(0.1),
+                    GridTrack::flex(0.1),
+                ],
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                ..default()
+            });
+
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                background_color: BackgroundColor(Color::linear_rgb(255.0, 255.0, 255.0)),
+                ..default()
+            });
+
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                ..default()
+            });
+
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                background_color: BackgroundColor(Color::linear_rgb(255.0, 255.0, 255.0)),
+                ..default()
+            });
+        });
+}
